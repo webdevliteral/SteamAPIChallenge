82ed148bdaa8a2d13891444acc9788fa06bef2cb
Devin Wood <pro.devinwood@gmail.com>
2021-01-15 00:11:33 -0500

    Fixed some of the layout and removed redundant pages

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { AboutComponent } from './about/about.component';
 import { SteamsearchComponent } from './steamsearch/steamsearch.component';
 
 const routes: Routes = [
-  { path: 'portfolio', component: PortfolioComponent },
-  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'steamsearch', component: SteamsearchComponent}
 ];
